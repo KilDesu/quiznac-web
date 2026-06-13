5e95026f4d11c1146cbac617d3d4c6708df58f23
Kilian Davies <contact.quiznac@gmail.com>
2026-06-13 16:24:59 +0200

    refactor: simplify image validation logic in addImageToDb to allow more types, such as base64 data

--- app/utils/imgbb.ts
+++ app/utils/imgbb.ts
@@ -38,16 +38,8 @@ export interface ImgbbResponse {
 }
 
 export async function addImageToDb(image: string | File) {
-  if (typeof image === "string") {
-    if (image.startsWith("https://i.ibb.co")) {
-      return image;
-    }
-
-    if (!image.startsWith("http")) {
-      throw new Error(
-        `L'URL de l'image ${image} n'est pas valide (elle doit commencer par "http")`,
-      );
-    }
+  if (typeof image === "string" && image.startsWith("https://i.ibb.co")) {
+    return image;
   }
 
   const apiKey = import.meta.env.VITE_IMGBB_API_KEY;
